41737b6ed313cf06bd212a81a551c359a7471490
rizkyark <rizkyark99@gmail.com>
2022-11-08 00:37:58 +0700

    add: set up midtrans

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dnhoxflfj",
+  api_key: "338328458243391",
+  api_secret: "KoUWJyePt3xJ22iI-0HSjW5txfA",
   secure: true,
 });
 
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -371,6 +371,13 @@ module.exports = {
         phoneNumber,
       };
 
+      // eslint-disable-next-line no-restricted-syntax
+      for (const data in updateData) {
+        if (!updateData[data]) {
+          delete updateData[data];
+        }
+      }
+
       await authModel.updateProfile(id, updateData);
       const result = await authModel.getUserByID(id);
 
